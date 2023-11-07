a9fa113f3b32d5777706f82344ad79a48896be6a
zansari18 <143058076+zansari18@users.noreply.github.com>
2023-11-07 13:21:34 -0600

    Added files
    I'm sorry, I directly open the html files to my browser to see my output. I added My name to the html and my email. And I edited the javascript to be able to take an entry and rewrite the header as well as give an alert with the given text entry

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 function init(){
 //add your javascrip between these two lines of code
+ var button=document.getElementById("entrybutton");
  
+ function showText (){
+	 var textentry=document.getElementById("entryinput").value;
+	 var output=document.getElementById("textoutput");
+	 output.innerHTML = textentry;
+     alert("Zuha Ansari: "+ textentry);
+    }
 
-
-
-
+ button.addEventListener('click',showText);
+}
 
 
 
